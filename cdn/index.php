15f5fd6e47669226b85fba8ad503e852071b55b4
fbt0wizard <86288906+fbt0wizard@users.noreply.github.com>
2021-12-14 11:18:37 -0800

    invent

---
+++ cdn/index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ishoot Studio</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+</head>
+<body>
+    <header>
+    <div class="slide-images">
+        <div class="crossfade">
+            <figure></figure>
+            <figure></figure>
+            <figure></figure>
+            <figure></figure>
+            <figure></figure>
+        </div>
+    </div>
+        <div class="navbar">
+            <div class="site-logo">
+                <img src="images/logo.png" alt="site logo" width="70px">
+            </div>
+            <div class="nav-target">
+                <ul class="nav-ul">
+                    <li class="navlinks"><a href="#">Home</a></li>
+                    <li class="navlinks droper"><a href="#">Weddings <span><i class="fa fa-chevron-circle-down" aria-hidden="true"></i></span></a></li>
+                    <ul class="dropdown absolute1">
+                        <li class="mini-navlinks"><a href="#">Pre-wedding Photography</a></li>
+                        <li class="mini-navlinks"><a href="#">wedding Photography</a></li>
+                    </ul>
+                    <li class="navlinks"><a href="#">Event Photography</a></li>
+                    <li class="navlinks droper"><a href="#">Portraits <span><i class="fa fa-chevron-circle-down" aria-hidden="true"></i></span></a></li>
+                    <ul class="dropdown absolute2">
+                        <li class="mini-navlinks"><a href="#">Headshot Photography</a></li>
+                        <li class="mini-navlinks"><a href="#">Portrait Photography</a></li>
+                        <li class="mini-navlinks"><a href="#">Family Photography</a></li>
+                        <li class="mini-navlinks"><a href="#">Children Photography</a></li>
+                    </ul>
+                    <li class="navlinks droper"><a href="#">Commercial <span><i class="fa fa-chevron-circle-down" aria-hidden="true"></i></span></a></li>
+                    <ul class="dropdown absolute3">
+                        <li class="mini-navlinks"><a href="#">Modeling Photography</a></li>
+                        <li class="mini-navlinks"><a href="#">Fashion Photography</a></li>
+                        <li class="mini-navlinks"><a href="#">Product Shoot</a></li>
+                        <li class="mini-navlinks"><a href="#">Product Frame</a></li>
+                    </ul>
+                    <li class="navlinks"><a href="#">Blog</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div class="description">
+        <div class="content">
+            <h2 class="heading">IshootStudio Nigeria</h2>
+            <h4 class="more-content">
+            Ishoot studio Nigeria is a trustworthy photography firm that has been operating since 
+            2015 with the main goal of creating memories with creative ideas and technical know-how. 
+            We specialized in portrait photography, wedding photography, modeling photoshoot, product photography,
+            children photography, Family photography, training, and more.
+            </h4>
+        </div>
+    </div>
+
+    <div class="cards">
+        <div class="flex-container">
+            <div class="flex-item-left">
+                <div class="card">
+                    <a href="#"><img src="images/8.jpg" alt="Avatar" style="width:100%"></a>
+                    <div class="container">
+                        <h4><b>Tittle</b></h4> 
+                        <p>Event Details</p> 
+                    </div>
+                </div>
+            </div>
+            <div class="flex-item-right">
+            <div class="card">
+                    <a href="#"><img src="images/7.jpg" alt="Avatar" style="width:100%"></a>
+                    <div class="container">
+                        <h4><b>Tittle</b></h4> 
+                        <p>Event Details</p> 
+                    </div>
+                </div>
+            </div>
+            <div class="flex-item-left">
+            <div class="card">
+                    <a href="#"><img src="images/12.jpg" alt="Avatar" style="width:100%"></a>
+                    <div class="container">
+                        <h4><b>Tittle</b></h4> 
+                        <p>Event Details</p> 
+                    </div>
+                </div>
+            </div>
+            <div class="flex-item-right">
+            <div class="card">
+                    <a href="#"><img src="images/10.jpg" alt="Avatar" style="width:100%"></a>
+                    <div class="container">
+                        <h4><b>Tittle</b></h4> 
+                        <p>Event Details</p> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="grid-photos">
+        <h2 class="heading portfolio">
+        Photography Portfolio
+        </h2>
+        <h4 class="more-content portfolio-content">
+        If you are looking for a professional photographer to document your
+         memories in a unique and professional way, Ishootstudio offers you the best experience.
+        </h4>
+        <div class="row"> 
+            <div class="column">
+                <img src="images/1.jpg" style="width:100%">
+                <img src="images/2.jpg" style="width:100%">
+                <img src="images/12.jpg" style="width:100%">
+                <img src="images/4.jpg" style="width:100%">
+                <img src="images/10.jpg" style="width:100%">
+                <img src="images/6.jpg" style="width:100%">
+                <img src="images/7.jpg" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="images/12.jpg" style="width:100%">
+                <img src="images/9.jpg" style="width:100%">
+                <img src="images/3.jpg" style="width:100%">
+                <img src="images/11.jpg" style="width:100%">
+                <img src="images/5.jpg" style="width:100%">
+                <img src="images/8.jpg" style="width:100%">
+                <img src="images/2.jpg" style="width:100%">
+            </div>  
+            <div class="column">
+                <img src="images/8.jpg" style="width:100%">
+                <img src="images/11.jpg" style="width:100%">
+                <img src="images/3.jpg" style="width:100%">
+                <img src="images/10.jpg" style="width:100%">
+                <img src="images/5.jpg" style="width:100%">
+                <img src="images/7.jpg" style="width:100%">
+                <img src="images/2.jpg" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="images/7.jpg" style="width:100%">
+                <img src="images/10.jpg" style="width:100%">
+                <img src="images/3.jpg" style="width:100%">
+                <img src="images/12.jpg" style="width:100%">
+                <img src="images/5.jpg" style="width:100%">
+                <img src="images/8.jpg" style="width:100%">
+                <img src="images/2.jpg" style="width:100%">
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+
+    </footer>
+    <script src="cdn/jquery-3.6.0.min.js"></script>
+    <script src="cdn/sweetalert2.all.min.js"></script>
+    <script src="index.js"></script>
+</body>
+</html>

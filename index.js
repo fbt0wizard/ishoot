2be0c6c7fc51c76d39ef317b4b699c92bf712866
fbt0wizard <86288906+fbt0wizard@users.noreply.github.com>
2021-12-31 00:08:38 -0800

    modal

--- index.js
+++ index.js
@@ -17,3 +17,42 @@
       });
     }); 
   })(jQuery);
+
+
+//   // Get the modal
+// var modal = document.getElementById('myModal');
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = $('.myImg');
+// var modalImg = $("#img01");
+// var captionText = document.getElementById("caption");
+// $('.myImg').click(function(){
+//     modal.style.display = "block";
+//     var newSrc = this.src;
+//     modalImg.attr('src', newSrc);
+//     captionText.innerHTML = this.alt;
+// });
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+
+
+var height = 700;
+var img;
+
+function openImg(event) {
+  img = event.target.src;
+  // console.log(event.target.parentNode)
+  Swal.fire({
+    imageUrl: img,
+    imageAlt: 'A tall image',
+    imageWidth: '100%',
+    showConfirmButton: false
+  })
+}
